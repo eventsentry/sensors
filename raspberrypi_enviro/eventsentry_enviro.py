1b501ae0ff1d7b0cec94a8307ab6b5ebbfb3fa91
netikus <84867531+netikus@users.noreply.github.com>
2021-11-15 12:56:23 -0600

    Add files via upload
    Added support for LCD output

--- raspberrypi_enviro/eventsentry_enviro.py
+++ raspberrypi_enviro/eventsentry_enviro.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
+########################################
+# These values can be adjusted and only
+# affect the LCD display
+
+thresholdTemp = 80
+
+fontSize = 17
+lcdColorBackground = (0, 170, 170)
+lcdColorTempHigh = (255, 0, 0)
+########################################
+
+import ST7735
+import socket
+import sys
+import logging
 import time
+
 from bme280 import BME280
+from PIL import Image, ImageDraw, ImageFont
+from fonts.ttf import RobotoMedium as UserFont
 
 try:
     from smbus2 import SMBus
@@ -15,14 +33,32 @@ try:
 except ImportError:
     import ltr559
 
-import logging
+# Get IP address
+def getIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
+# Get temperature, adjusted for CPU heat
 def get_cpu_temperature():
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
         temp = f.read()
         temp = int(temp) / 1000.0
         temp = temp * 9.0/5.0+32
     return temp
+
+# Display text on LCD
+def lcdShowText(x, y, bgColor, text):
+    draw.rectangle((0, 0, 160, 80), bgColor)
+    draw.text((x, y), text, font=font, fill=text_colour)
+    disp.display(img)
 
 # Tuning factor for compensation. Decrease this number to adjust the
 # temperature down, and increase to adjust up
@@ -35,11 +71,60 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-logging.info("""eventsentry.py - Prints and stores temperature, humidity and light readings from sensor
+logging.info("""eventsentry_enviro.py - Prints and stores temperature, humidity and light readings from sensor
 
 Press Ctrl+C to exit!
-
 """)
+
+# Get hostname and IP
+hostName = socket.gethostname()
+hostIP = getIP()
+
+# ######################################
+# Create LCD class instance & initialize
+disp = ST7735.ST7735(
+    port=0,
+    cs=1,
+    dc=9,
+    backlight=12,
+    rotation=270,
+    spi_speed_hz=10000000
+)
+disp.begin()
+WIDTH = disp.width
+HEIGHT = disp.height
+
+# Display EventSentry logo
+image = Image.open('eventsentry_enviro.gif')
+frame = 0
+
+while True:
+    try:
+        image.seek(frame)
+        disp.display(image.resize((WIDTH, HEIGHT)))
+        frame += 1
+        time.sleep(0.05)
+
+    except EOFError:
+        break
+
+time.sleep(2)
+
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+draw = ImageDraw.Draw(img)
+
+font = ImageFont.truetype(UserFont, fontSize)
+text_colour = (255, 255, 255)
+
+message = hostName + "\n" + hostIP
+
+size_x, size_y = draw.textsize(message, font)
+
+# Display host name and IP address
+lcdShowText(0, 0, lcdColorBackground, message)
+
+time.sleep(2)
+# ######################################
 
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
@@ -80,6 +165,13 @@ Relative humidity: {:05.2f} %
 Light: {:05.2f} Lux
 Proximity: {:05.2f} Unfiltered: {:05.2f}
 """.format(temperature_adj, pressure, humidity, lux, proximity, proximity_raw))
+
+    # Display stats on LCD
+    lcdBackground = lcdColorBackground
+    message = "{}\nTemp: {:.1f}F\nHumidity: {:.1f}%\nLight: {:.1f}lux".format(time.strftime('%H:%M:%S'), temperature_adj, humidity, lux)
+    if temperature_adj >= thresholdTemp:
+        lcdBackground = lcdColorTempHigh
+    lcdShowText(0, 0, lcdBackground, message)
 
     with open('/tmp/es_temperature.txt', 'w') as f:
         f.write("{:.0f}".format(temperature_adj))
